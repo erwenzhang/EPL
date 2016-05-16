ce5587ba2e13a8f02cb6e041adfabd27458ec888
erwenzhang <zhangvivien@126.com>
2016-05-16 02:26:41 -0500

    Update 2010midterm.cpp

--- 2010midterm.cpp
+++ 2010midterm.cpp
@@ -103,3 +103,51 @@ int main(void){
 
 }
 #endif
+
+#ifdef prob15//2010 midterm 7
+using std::cout;
+template<typename Op1,typename Op2,typename T>
+class TwoProxy;
+
+template<typename Op,typename T>
+
+class Proxy{
+	
+public:
+	Op op;
+	T val;
+	Proxy(Op _op, T _val) :op(_op), val(_val){}
+	T operator()(const T& rhs){ return op(val, rhs); }
+	template<typename Op2>
+	TwoProxy<Op,Op2,T> operator+(Proxy<Op2,T>& rhs){
+		return TwoProxy<Op, Op2, T>{*this, rhs};
+	}
+
+
+};
+
+template<typename Op1,typename Op2,typename T>
+class TwoProxy{
+	Op1 op1;
+	Op2 op2;
+	T val1;
+	T val2;
+public:
+	TwoProxy(Proxy<Op1, T>lhs, Proxy<Op2, T>rhs) :op1(lhs.op), val1(lhs.val), op2(rhs.op), val2(rhs.val){}
+	T operator()(const T& arg){ return op2(val2, op1(val1, arg)); }
+
+
+};
+template<typename Op,typename T>
+Proxy<Op,T> curry(Op op, T val){
+	return Proxy<Op,T>{op, val};
+}
+
+
+int main(void){
+	cout<<(curry(std::multiplies<int>(), 5)+curry(std::plus<int>(),3))(5);
+
+	getchar();
+
+}
+#endif
